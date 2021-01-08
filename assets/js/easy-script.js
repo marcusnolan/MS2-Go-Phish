e2b4bc450bdf3aca61ac8a712ee1a662d80579e5
marcusnolan <marcusnolan130@gmail.com>
2021-01-08 16:05:51 +0000

    feat - 'How To Play' overlay added to game-easy.html

---
+++ assets/js/easy-script.js
@@ -0,0 +1,7 @@
+function openHowToPlay() {
+  document.getElementById("howToPlay").style.height = "100%";
+}
+
+function closeHowToPlay() {
+  document.getElementById("howToPlay").style.height = "0%";
+}
