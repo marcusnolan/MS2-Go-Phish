81c91adf841d10792681fa0e130b68911b4a7525
marcusnolan <marcusnolan130@gmail.com>
2021-01-11 13:58:22 +0000

    feat - basic code for game added

--- assets/js/easy-script.js
+++ assets/js/easy-script.js
@@ -1,3 +1,71 @@
+const cards = document.querySelectorAll('.go-phish-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+
+        return;
+    }
+
+    secondCard = this;
+
+    checkForMatch();
+}
+//Function to check if both cards clicked have the same datatset given in HTML (are a pair). 
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+//Function below removes flip if a match has been made
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+//Function below unflips cards if no match is made
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
+
+//Function below resets the deck
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+//Function below shuffles the board before the start of the game
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+
 function openHowToPlay() {
   document.getElementById("howToPlay").style.height = "100%";
 }
@@ -5,3 +73,4 @@ function openHowToPlay() {
 function closeHowToPlay() {
   document.getElementById("howToPlay").style.height = "0%";
 }
+
